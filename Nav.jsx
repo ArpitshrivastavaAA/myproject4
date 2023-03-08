b3f31ff7fccc82b46cef4ddeebd02048af4b078d
ArpitshrivastavaAA <120470221+ArpitshrivastavaAA@users.noreply.github.com>
2023-03-08 22:24:35 +0530

    Add files via upload

--- Nav.jsx
+++ Nav.jsx
@@ -11,18 +11,50 @@
 
 // export default Nav
 
-import React from "react";
-import {Component} from "react"
+// import React from "react";
+// import {Component} from "react"
 
-class Nav extends Component
-{
-    render()
-    {
-        return( <>
-            <h1>{this.props.data}</h1>
-            <h2>{this.props.data2}</h2>
-            </>
-        )
-    }
+// class Nav extends Component
+// {
+//     render()
+//     {
+//         return( <>
+//             <h1>{this.props.data}</h1>
+//             <h2>{this.props.data2}</h2>
+//             </>
+//         )
+//     }
+// }
+// export default Nav
+
+// import React from "react";
+// import {Component} from "react"
+
+// class Nav extends Component
+// {
+//     render()
+//     {
+//         return( <>
+//             <h1>{this.props.string}</h1>
+//             <h2>{this.props.number}</h2>
+//             <h1>{this.props.boolean}</h1>
+//             <h2>{this.props.data}</h2>
+//             </>
+//         )
+//     }
+// }
+// export default Nav
+
+
+import React from "react";
+
+const Nav =(props) => {
+    return (
+<>
+<img src={props.photo} alt="" />
+
+</>
+
+    )
 }
 export default Nav
