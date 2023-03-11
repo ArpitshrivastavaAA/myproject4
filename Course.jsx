aed8b3a43af74b14a6853711be83c94073be758f
ArpitshrivastavaAA <120470221+ArpitshrivastavaAA@users.noreply.github.com>
2023-03-11 12:02:37 +0530

    Add files via upload

---
+++ Course.jsx
@@ -0,0 +1,263 @@
+
+
+// import React from 'react'
+
+// const Course = (props) => {
+    
+//   return (
+    
+//     <div className ="parentBlock">
+//       <section>
+//         <div className='mainBlock'>
+//         <img src = {props.payload[0].photo} alt=""/>
+//         <h2>{props.payload[0].studentName}</h2>
+//         <h3>{props.payload[0].collegeName}</h3>
+//         <h4>{props.payload[0].stream}</h4>
+//         <h4>{props.payload[0].yop}</h4>
+//         <h4>{props.payload[0].duration}</h4>
+        
+          
+//         </div>
+//         <div className='mainBlock'>
+//         <img src = {props.payload[1].photo} alt=""/>
+//         <h2>{props.payload[1].studentName}</h2>
+//         <h3>{props.payload[1].collegeName}</h3>
+//         <h4>{props.payload[1].stream}</h4>
+//         <h4>{props.payload[1].yop}</h4>
+//         <h4>{props.payload[1].duration}</h4>
+       
+//         </div>
+       
+//         <div className='mainBlock'>
+//         <img src = {props.payload[2].photo} alt=""/>
+//         <h2>{props.payload[2].studentName}</h2>
+//         <h3>{props.payload[2].collegeName}</h3>
+//         <h4>{props.payload[2].stream}</h4>
+//         <h4>{props.payload[2].yop}</h4>
+//         <h4>{props.payload[2].duration}</h4>
+        
+//         </div>
+          
+//         <div className='mainBlock'>
+//         <img src = {props.payload[3].photo} alt=""/>
+//         <h2>{props.payload[3].studentName}</h2>
+//         <h3>{props.payload[3].collegeName}</h3>
+//         <h4>{props.payload[3].stream}</h4>
+//         <h4>{props.payload[3].yop}</h4>
+//         <h4>{props.payload[3].duration}</h4>
+        
+//         </div>
+//         </section>
+        
+    
+//              <section>
+//         <div className='mainBlock'>
+//         <img src = {props.payload[4].photo} alt=""/>
+//         <h2>{props.payload[4].studentName}</h2>
+//         <h3>{props.payload[4].collegeName}</h3>
+//         <h4>{props.payload[4].stream}</h4>
+//         <h4>{props.payload[4].yop}</h4>
+//         <h4>{props.payload[4].duration}</h4>
+      
+
+//         </div>
+
+//         <div className='mainBlock'>
+//         <img src = {props.payload[5].photo} alt=""/>
+//         <h2>{props.payload[5].studentName}</h2>
+//         <h3>{props.payload[5].collegeName}</h3>
+//         <h4>{props.payload[5].stream}</h4>
+//         <h4>{props.payload[5].yop}</h4>
+//         <h4>{props.payload[5].duration}</h4>
+       
+//         </div>
+        
+
+//         <div className='mainBlock'>
+//         <img src = {props.payload[6].photo} alt=""/>
+//         <h2>{props.payload[6].studentName}</h2>
+//         <h3>{props.payload[6].collegeName}</h3>
+//         <h4>{props.payload[6].stream}</h4>
+//         <h4>{props.payload[6].yop}</h4>
+//         <h4>{props.payload[6].duration}</h4>
+        
+//         </div>
+
+//         <div className='mainBlock'>
+//         <img src = {props.payload[7].photo} alt=""/>
+//         <h2>{props.payload[7].studentName}</h2>
+//         <h3>{props.payload[7].collegeName}</h3>
+//         <h4>{props.payload[7].stream}</h4>
+//         <h4>{props.payload[7].yop}</h4>
+//         <h4>{props.payload[7].duration}</h4>
+        
+
+//         </div>
+//         </section>
+//         <section>
+
+//         <div className='mainBlock'>
+//         <img src = {props.payload[8].photo} alt=""/>
+//         <h2>{props.payload[8].studentName}</h2>
+//         <h3>{props.payload[8].collegeName}</h3>
+//         <h4>{props.payload[8].stream}</h4>
+//         <h4>{props.payload[8].yop}</h4>
+//         <h4>{props.payload[8].duration}</h4>
+       
+//         </div>
+        
+        
+//         <div className='mainBlock'>
+//         <img src = {props.payload[9].photo} alt=""/>
+//         <h2>{props.payload[9].studentName}</h2>
+//         <h3>{props.payload[9].collegeName}</h3>
+//         <h4>{props.payload[9].stream}</h4>
+//         <h4>{props.payload[9].yop}</h4>
+//         <h4>{props.payload[9].duration}</h4>
+        
+
+//         </div>
+
+//         <div className='mainBlock'>
+//         <img src = {props.payload[10].photo} alt=""/>
+//         <h2>{props.payload[10].studentName}</h2>
+//         <h3>{props.payload[10].collegeName}</h3>
+//         <h4>{props.payload[10].stream}</h4>
+//         <h4>{props.payload[10].yop}</h4>
+//         <h4>{props.payload[10].duration}</h4>
+        
+
+//         </div>
+
+//         <div className='mainBlock'>
+//         <img src = {props.payload[11].photo} alt=""/>
+//         <h2>{props.payload[11].studentName}</h2>
+//         <h3>{props.payload[11].collegeName}</h3>
+//         <h4>{props.payload[11].stream}</h4>
+//         <h4>{props.payload[11].yop}</h4>
+//         <h4>{props.payload[11].duration}</h4>
+       
+//         </div>
+//         </section>
+        
+        
+//         <section>
+
+//         <div className='mainBlock'>
+//         <img src = {props.payload[12].photo} alt=""/>
+//         <h2>{props.payload[12].studentName}</h2>
+//         <h3>{props.payload[12].collegeName}</h3>
+//         <h4>{props.payload[12].stream}</h4>
+//         <h4>{props.payload[12].yop}</h4>
+//         <h4>{props.payload[12].duration}</h4>
+       
+
+//         </div>
+      
+      
+//         <div className='mainBlock'>
+//         <img src = {props.payload[13].photo} alt=""/>
+//         <h2>{props.payload[13].studentName}</h2>
+//         <h3>{props.payload[13].collegeName}</h3>
+//         <h4>{props.payload[13].stream}</h4>
+//         <h4>{props.payload[13].yop}</h4>
+//         <h4>{props.payload[13].duration}</h4>
+        
+//         </div>
+
+//         <div className='mainBlock'>
+//         <img src = {props.payload[14].photo} alt=""/>
+//         <h2>{props.payload[14].studentName}</h2>
+//         <h3>{props.payload[14].collegeName}</h3>
+//         <h4>{props.payload[14].stream}</h4>
+//         <h4>{props.payload[14].yop}</h4>
+//         <h4>{props.payload[14].duration}</h4>
+       
+//         </div>
+
+//         <div className='mainBlock'>
+//         <img src = {props.payload[15].photo} alt=""/>
+//         <h2>{props.payload[15].studentName}</h2>
+//         <h3>{props.payload[15].collegeName}</h3>
+//         <h4>{props.payload[15].stream}</h4>
+//         <h4>{props.payload[15].yop}</h4>
+//         <h4>{props.payload[15].duration}</h4>
+        
+//         </div>
+//         </section>
+       
+       
+//         <section>
+//         <div className='mainBlock'>
+//         <img src = {props.payload[16].photo} alt=""/>
+//         <h2>{props.payload[16].studentName}</h2>
+//         <h3>{props.payload[16].collegeName}</h3>
+//         <h4>{props.payload[16].stream}</h4>
+//         <h4>{props.payload[16].yop}</h4>
+//         <h4>{props.payload[16].duration}</h4>
+        
+//         </div>
+      
+//         <div className='mainBlock'>
+//         <img src = {props.payload[17].photo} alt=""/>
+//         <h2>{props.payload[17].studentName}</h2>
+//         <h3>{props.payload[17].collegeName}</h3>
+//         <h4>{props.payload[17].stream}</h4>
+//         <h4>{props.payload[17].yop}</h4>
+//         <h4>{props.payload[17].duration}</h4>
+        
+//         </div>
+
+//         <div className='mainBlock'>
+//         <img src = {props.payload[18].photo} alt=""/>
+//         <h2>{props.payload[18].studentName}</h2>
+//         <h3>{props.payload[18].collegeName}</h3>
+//         <h4>{props.payload[18].stream}</h4>
+//         <h4>{props.payload[18].yop}</h4>
+//         <h4>{props.payload[18].duration}</h4>
+        
+//         </div>
+//         <div className='mainBlock'>
+//         <img src = {props.payload[19].photo} alt=""/>
+//         <h2>{props.payload[19].studentName}</h2>
+//         <h3>{props.payload[19].collegeName}</h3>
+//         <h4>{props.payload[19].stream}</h4>
+//         <h4>{props.payload[19].yop}</h4>
+//         <h4>{props.payload[19].duration}</h4>
+//         </div>
+//         </section>
+
+//         </div>
+      
+    
+//   )
+// }
+
+// export default Course
+
+import React from "react";
+const Course = (props) => {
+  if(props.CourseDetails.CourseAvail == true)
+  {
+    return(
+      <>
+      <h1>{props.CourseDetails.CourseName}</h1>
+      <h2>{props.CourseDetails.CourseDuration}</h2>
+      <button style = {{color : "green"}}>AVAILABLE</button>
+      </>
+
+    )
+  }
+  else
+  {
+    return(
+      <>
+      <h1>{props.CourseDetails.CourseName}</h1>
+      <h2>{props.CourseDetails.CourseDuration}</h2>
+      <button style = {{color : "green"}}> NOT AVAILABLE</button>
+      </>
+
+    )
+  }
+} 
+export  default Course
